1e8650ccd3fa6acfea5353e8a509060958c097ab
Nick Warwick <nicholaswarwick@icloud.com>
2019-09-13 08:33:22 -0400

    Formatting

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,33 @@
-var zoomLevel = 2;
+var zoomLevel = 2
 
-mapboxgl.accessToken = 'pk.eyJ1IjoibndhcndpY2siLCJhIjoiY2owYWR6NnZoMDA3NTMzb2F3aGQ2YXpvZyJ9.vQzH-hYOzRMurslNpAfiSg';
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibndhcndpY2siLCJhIjoiY2owYWR6NnZoMDA3NTMzb2F3aGQ2YXpvZyJ9.vQzH-hYOzRMurslNpAfiSg'
 var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/dark-v9', //stylesheet location
-    center: [0, 0], // starting position
-    zoom: zoomLevel // starting zoom
-});
+  container: 'map', // container id
+  style: 'mapbox://styles/mapbox/dark-v9', //stylesheet location
+  center: [0, 0], // starting position
+  zoom: zoomLevel // starting zoom
+})
 
 map.on('load', function() {
-    map.addSource('quakes', {
-        type: 'geojson',
-        data: 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'
-    });
-    map.addLayer({
-        'id': 'quakes',
-        'type': 'circle',
-        'source': 'quakes',
-        'layout': {
-            'visibility': 'visible'
-        },
-        'paint': {
-            'circle-radius': {
-                property: 'mag',
-                stops: [
-                    [1, 1],
-                    [2, 2],
-                    [3, 4],
-                    [4, 8],
-                    [5, 16],
-                    [6, 32],
-                    [7, 64],
-                ]
-            },
-            'circle-color': 'rgb(0, 249, 124)'
-        },
-    });
-});
+  map.addSource('quakes', {
+    type: 'geojson',
+    data:
+      'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'
+  })
+  map.addLayer({
+    id: 'quakes',
+    type: 'circle',
+    source: 'quakes',
+    layout: {
+      visibility: 'visible'
+    },
+    paint: {
+      'circle-radius': {
+        property: 'mag',
+        stops: [[1, 1], [2, 2], [3, 4], [4, 8], [5, 16], [6, 32], [7, 64]]
+      },
+      'circle-color': 'rgb(0, 249, 124)'
+    }
+  })
+})
